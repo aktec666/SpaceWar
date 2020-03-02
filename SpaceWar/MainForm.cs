d7e69e65dfcc3ffa01a6306f70fcf685644960c3
aktec <aktec@mail.ru>
2020-03-02 12:00:58 +0300

    Добавили messagebox

--- SpaceWar/MainForm.cs
+++ SpaceWar/MainForm.cs
@@ -16,5 +16,10 @@ namespace SpaceWar
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello!");
+        }
     }
 }
